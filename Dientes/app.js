8341ecfbbc4dd8720d28f7817f4e0fdfaa6c7bc9
Luis Ángel Peraza Lugo <perazalugo1@gmail.com>
2023-11-22 12:22:25 -0700

    Cambios

--- Dientes/app.js
+++ Dientes/app.js
@@ -1,19 +1,28 @@
-// Hacer arrastrable el cepillo y detectar la colisión con los dientes
-Draggable.create(".cepillo", {
-    bounds: "body",
-    onDrag: function () {
-        // Obtener la posición del cepillo al arrastrar
-        let cepilloRect = this.target.getBoundingClientRect();
-        let dientesRect = document.querySelector('.dientes').getBoundingClientRect();
-
-        // Detectar la colisión usando la función hitTest de GSAP
-        if (this.hitTest('.dientes')) {
-
-            let progresoLimpieza = (cepilloRect.right - dientesRect.left) / (dientesRect.width);    // Calcular el progreso de limpieza basado en la posición del cepillo
-            document.querySelector('.dientesLimpios').style.opacity = progresoLimpieza; // Ajustar la opacidad de los dientes limpios gradualmente
-        } else {
-            // Si no se superpone, ocultar los dientes limpios
-            document.querySelector('.dientesLimpios').style.opacity = 0;
-        }
+let cepilloMovible = false; // Variable para controlar si el cepillo se puede mover
+Draggable.create(".pasta", { // Hacer arrastrable el cepillo y detectar la colisión con los dientes
+  bounds: "body",
+  onDrag: function () {   // Establecer la variable cepilloMovible a true si la pasta de dientes pasa por el cepillo
+    if (this.hitTest('.cepillo')) {
+      cepilloMovible = true;
     }
+  }
+});
+
+Draggable.create(".cepillo", { // Hacer arrastrable el cepillo y detectar la colisión con los dientes
+  bounds: "body",
+  draggable: false, // Bloquear el movimiento del cepillo
+  onDrag: function () {
+    if (cepilloMovible) {
+      let cepilloRect = this.target.getBoundingClientRect();    // Obtener la posición del cepillo al arrastrar
+      let dientesRect = document.querySelector('.dientes').getBoundingClientRect();
+
+      if (this.hitTest('.dientes')) {  // Detectar la colisión entre el cepillo y los dientes
+        let progresoLimpieza = (cepilloRect.right - dientesRect.left) / (dientesRect.width); // Calcular el progreso de limpieza basado en la posición del cepillo
+        console.log(progresoLimpieza);
+        document.querySelector('.dientesLimpios').style.opacity = progresoLimpieza; // Ajustar la opacidad de los dientes limpios gradualmente
+      } else {
+        document.querySelector('.dientesLimpios').style.opacity = 0;  // Si no hay colisión, ocultar los dientes limpios
+      }
+    }
+  }
 });
