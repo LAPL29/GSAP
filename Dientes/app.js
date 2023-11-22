0fd7bb3192df77cc2b347cf4b094f4a045e40734
lapl29 <perazalugo1@gmail.com>
2023-11-22 06:20:53 -0700

    Cambios
    aaa

--- Dientes/app.js
+++ Dientes/app.js
@@ -1,16 +1,19 @@
-let elementsToDrag = document.querySelectorAll('.cepillo');
-
-// Hacer arrastrables solo los elementos seleccionados
-Draggable.create(elementsToDrag, {
+// Hacer arrastrable el cepillo y detectar la colisión con los dientes
+Draggable.create(".cepillo", {
     bounds: "body",
-    onRelease: function () {
-        var targetElement = this.target; // Guarda la referencia al elemento .box
-        if (this.hitTest("#dentadura") && targetElement.classList.contains('cepillo')) {
-            dentadura.style.backgroundColor = "blue";
-            this.endDrag();
+    onDrag: function () {
+        // Obtener la posición del cepillo al arrastrar
+        let cepilloRect = this.target.getBoundingClientRect();
+        let dientesRect = document.querySelector('.dientes').getBoundingClientRect();
+
+        // Detectar la colisión usando la función hitTest de GSAP
+        if (this.hitTest('.dientes')) {
+
+            let progresoLimpieza = (cepilloRect.right - dientesRect.left) / (dientesRect.width);    // Calcular el progreso de limpieza basado en la posición del cepillo
+            document.querySelector('.dientesLimpios').style.opacity = progresoLimpieza; // Ajustar la opacidad de los dientes limpios gradualmente
         } else {
-            dentadura.style.backgroundColor = "white";
+            // Si no se superpone, ocultar los dientes limpios
+            document.querySelector('.dientesLimpios').style.opacity = 0;
         }
     }
 });
-
